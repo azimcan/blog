49184d89b235d0386da0efe5ce58fe8d330d01a6
Azim Can KURUCA <azimcankuruca@gmail.com>
2022-02-17 12:08:47 +0300

    Delete body column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_15_131809) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_17_090436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_15_131809) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "body"
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

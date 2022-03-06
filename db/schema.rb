dde32ab1693c432b256c0b30ec1f2a52fe0b2fc0
Azim Can KURUCA <azimcankuruca@gmail.com>
2022-03-06 13:11:44 +0300

    add test sample

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_23_142228) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

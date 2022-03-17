74e063fe805cc810d03947336f7e14a8d01594d7
Azim Can KURUCA <azimcankuruca@gmail.com>
2022-03-17 16:22:37 +0300

    Added stimulus alert

---
+++ app/javascript/controllers/alert_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.element.classList.remove("translate-x-full")
+  }
+
+  close() {
+    this.element.classList.add("translate-x-full")
+  }
+}
